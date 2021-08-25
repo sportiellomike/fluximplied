a48b0650e7659a108ce34f17e5ca60c42c3fd5d4
msport469 <sportiellomike@gmail.com>
2021-08-25 08:52:39 -0400

    Create fluxitup.R

---
+++ R/fluxitup.R
@@ -0,0 +1,11 @@
+#' fluxitup
+#'
+#' @return This function will return something that we can only hope will satisfy your wildest hopes and dreams.
+#' @export
+#'
+#' @examples
+#' fluxitup()
+art<-readLines("https://raw.githubusercontent.com/sportiellomike/fluximplieddev/master/what.txt")
+dput(art)
+
+fluxitup<- function() {print(art)}
