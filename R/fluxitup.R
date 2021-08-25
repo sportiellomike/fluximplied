c5b8c5146d2581d602af0579c5ab7aca4fe2590e
msport469 <sportiellomike@gmail.com>
2021-08-25 09:35:52 -0400

    rebuilt package for fluxitup

--- R/fluxitup.R
+++ R/fluxitup.R
@@ -1,11 +1,13 @@
 #' fluxitup
 #'
-#' @return This function will return something that we can only hope will satisfy your wildest hopes and dreams.
+#' @description This function will return something that we can only hope will satisfy your wildest hopes and dreams.
+#' @return Something for everyone.
 #' @export
 #'
 #' @examples
 #' fluxitup()
+fluxitup<- function() {print(art)}
 art<-readLines("https://raw.githubusercontent.com/sportiellomike/fluximplieddev/master/what.txt")
 dput(art)
 
-fluxitup<- function() {print(art)}
+
