bd90bfbc9777190dcdbe41290671f2bce13cb4b9
Mike Sportiello <54905943+sportiellomike@users.noreply.github.com>
2022-04-21 10:40:49 -0400

    Update fluximplied.R

--- R/fluximplied.R
+++ R/fluximplied.R
@@ -1,14 +1,14 @@
 #' fluximplied
 #'
 #' @description Pathway analysis of DESeq2 result or character vector of differentially expressed genes which also plots results.
-#' @param inputdat what you are using as your input data, either a dataframe with genes as the rownames, a column for LFC, and a column for padj values
+#' @param inputdat what you are using as your input data, either a data frame with genes as the rownames, a column for LFC, and a column for padj values
 #' @param species either mus or hsa
 #' @param geneformat either ENTREZ or symbol
 #' @param inputformat either df or vector
-#' @param padjcolname the name of the column in your dataframe, if applicable, that stores the padj values
+#' @param padjcolname the name of the column in your data frame, if applicable, that stores the padj values
 #' @param pcutoff the alpha threshold for your padjustadjust
 #'
-#' @return If a dataframe was supplied, it should also return a dataframe as well as a bar graph of the enriched pathways.
+#' @return If a data frame was supplied, it should also return a data frame as well as a bar graph of the enriched pathways.
 #' @export
 #'
 #' @examples
@@ -31,7 +31,7 @@ fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',inputformat='
   RLSpathways<-RLSdatabase$metabolic.reaction
   #add kegg pathway ids
   KEGGpathwayids<-RLSdatabase$kegg.pathway.id
-  #make a dataframe with those genes and pathways
+  #make a data frame with those genes and pathways
   RLS<-data.frame(RLSgenes,RLSpathways,KEGGpathwayids)
   #create the responses if there are any genes left after subsetting on your genes
   #that are also in our database for being rate limiting steps
@@ -66,8 +66,10 @@ fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',inputformat='
          significancetable<<-significancetable
          plottable<-significancetable
          plottable$genepath<-paste0(rownames(plottable),' (RLS of ',plottable$metabolicrxn,')')
-         ifelse (!require(ggplot2),stop("ggplot2 not installed. Install it from CRAN."),1+1)
-         ifelse (!require(viridis),stop("viridis not installed. Install it from CRAN."),1+1)
+         if (!require(ggplot2)) {
+           stop("ggplot2 is not installed. Install it from CRAN.")}
+         if (!require(viridis)) {
+           stop("viridis is not installed. Install it from CRAN.")}
          fluximpliedplot<<-ggplot(plottable, aes(x=reorder(genepath,log2FoldChange), y=log2FoldChange , label=log2FoldChange)) +
            geom_bar(stat='identity', aes(fill=padjadj), width=.5,position="dodge")  +
            scale_fill_viridis(end=.9) +
