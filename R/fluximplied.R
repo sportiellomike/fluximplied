815d856e373237caa7002b37b2b15b5ed46d13ca
Mike Sportiello <54905943+sportiellomike@users.noreply.github.com>
2022-04-25 15:48:40 -0400

    got rid of cats

--- R/fluximplied.R
+++ R/fluximplied.R
@@ -14,7 +14,6 @@
 #' @examples
 #' fluximplied(inputdat=exampleData,species='mmu',geneformat='SYMBOL',inputformat='df',padjcolname='adj_pvalue',pcutoff=0.05)
 fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',padjcolname='adj_pvalue', inputformat='df',LFCcolname='Log2FoldChange',pcutoff=0.05) {
-  cat('Directly after function starts ',inputformat)
   # load and install packages
   list.of.packages <- c("viridis",
                         "ggplot2",
@@ -35,11 +34,11 @@ fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',padjcolname='
   # if(inputformat %in% veclist) {
   #   inputformat<-'vector'
   #   }
-  cat('Directly after dflist is made and just before can we flux it up ',inputformat)
+  
   # ensure all the inputs are of correct format
   canwefluxitup(inputdat=inputdat,species=species,geneformat=geneformat,padjcolname=padjcolname,
                 LFCcolname=LFCcolname,pcutoff=pcutoff)
-  cat('Directly after canwefluxitup ',inputformat)
+ 
   # make sure their padjcolname is actually in the vector they supplied
   `%!in%` <- Negate(`%in%`) # First we have to define the 'not in' operator
   if(inputformat %in% dflist & padjcolname %!in% colnames(inputdat)) {
@@ -63,9 +62,6 @@ fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',padjcolname='
   RLS<-data.frame(RLSgenes,RLSpathways,KEGGpathwayids)
   #create the responses if there are any genes left after subsetting on your genes
   #that are also in our database for being rate limiting steps
-  cat('directlybefore line 68 ',inputformat)
-  cat(class(inputformat))
-  cat(str(inputformat))
   
   if(inputformat %in% veclist) {
     print('We are using your vector of genes as the inputdat')
@@ -79,8 +75,7 @@ fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',padjcolname='
                 stop("There are no genes in your set that reach significance according to your supplied pcutoff.")
                 }
   }
-   cat('directlybefore line 82 ',inputformat)
-  #subset the database to only include genes in your set
+    #subset the database to only include genes in your set
   subset<-subset(RLS,RLS$RLSgenes %in% inputdat)
   #change the column names so the user knows what each column actually is
   colnames(subset)<-c('RLS genes in your set','Pathway associated with gene','KEGG Pathway ID')
@@ -91,10 +86,8 @@ fluximplied <- function(inputdat,species='mmu',geneformat='SYMBOL',padjcolname='
          (paste('There are no genes in your set that are in our rate limiting step database. Make sure you gave the correct species (Mmu or Hsa only) and geneformat (Symbol or ENTREZID only). If you are using the interactive GUI, you should be uploading a dataframe with a column of p values, a column called log2FoldChange, and genes should be in the first column. If you are not using the GUI, you can use the dataframe from a seurat or DESeq2 result with genes as rownames, or a character vector of genes. Sorry about that. We are as sad as you.')),
                {(paste0('Your gene set has --------> ',lengthintersect,' <-------- genes that have been identified as encoding enzymes involved as rate-limiting steps in the gene set you provided. If you are running this from Rstudio or the command line (not the interactive app), your RLS genes are saved as myRLSgenes and a dataframe of genes and corresponding pathways is saved as myRLStable.'))})
   #save the outputs so the user can hold onto them and look at them
-   cat('directlybefore line 93 ',inputformat)
   myRLStable<<-subset
   myRLSgenes<<-intersect(RLS$RLSgenes,inputdat)
-   cat('directlybefore line 96 ',inputformat)
   #print the RLS database that has been subset to only include genes that are in user's list
   if(inputformat=='df'||inputformat=='DF'||inputformat=='Df'||inputformat=='dataframe'||inputformat=='Dataframe'||inputformat=='data.frame')
          {significancetable<-inputssubset
