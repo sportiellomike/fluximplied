b580dfa3acdc832a1ddfb2016715c9171019b8b9
sportiellomike <54905943+sportiellomike@users.noreply.github.com>
2023-12-30 10:02:03 -0800

    Update fluximplied.R

--- R/fluximplied.R
+++ R/fluximplied.R
@@ -106,7 +106,7 @@ library(viridis)
            stop("viridis is not installed. Install it from CRAN.")}
           # actually make the plot for the user
          fluximpliedplot<<-ggplot(plottable, aes(x=genepath, y=plottable$log2FoldChange)) +
-           geom_bar(stat='identity', aes(fill=padjadj), width=.5,position="dodge")  +
+           geom_col(aes(fill=padjadj), width=.5,position="dodge")  +
            scale_fill_viridis(end=.9) +
            labs(title= "Pathway analysis with 'fluximplied'",x='',y=bquote('Log'[2]('Fold Change')),fill=bquote('P'['adjadj'])) +
            theme(axis.title = element_text(size=12),
